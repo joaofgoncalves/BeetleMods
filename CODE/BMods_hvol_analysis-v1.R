02c7bf22b47a0487d899dc36b2f5f7d8189ce469
João Gonçalves <joaofgo@gmail.com>
2023-01-26 01:30:01 +0000

    Add new plots to hypervolumes

--- CODE/BMods_hvol_analysis-v1.R
+++ CODE/BMods_hvol_analysis-v1.R
@@ -7,6 +7,70 @@ library(ggVennDiagram)
 setwd("C:/MyFiles/R-dev/BeetleMods")
 
 
+
+ggVennDiagHvols <- function(hvRefs, hvCS, sp, vs, width = 5, height= 4, tl = 10000){
+  
+  hv_set <- hypervolume_set(hvRefs, hvCS, check.memory=FALSE, 
+                            num.points.max = 1E6)
+  
+
+  
+  uniall    <- hv_set@HVList$Union@Volume
+  int       <- hv_set@HVList$Intersection@Volume / uniall
+  fracUniq1 <- hv_set@HVList$Unique_1@Volume / uniall
+  fracUniq2 <- hv_set@HVList$Unique_2@Volume / uniall
+  
+  # hypervolume_overlap_statistics(hv_set)
+  
+  tl <- tl
+  
+  fint <- 1:(round(int*tl))
+  len_fint <- length(fint)
+  
+  f1 <- c(fint,(len_fint + 1):(len_fint + round(fracUniq1*tl)))
+  len_f1 <- length(f1)
+  
+  f2 <- c(fint, (len_f1 + len_fint + 1) : (len_f1 + len_fint + round(fracUniq2*tl)))
+  
+  ggv <- ggVennDiagram(list(`Other sources` = f1,
+                            `Citizen Science` = f2), label = "percent", label_percent_digit=2) + 
+    scale_fill_distiller(palette = "Paired") +
+    labs(title = paste("\n",sp," | Venn diagram - hipervolume fractions\n"),sep="") +
+    theme(legend.position = "none") + 
+    theme(panel.background = element_rect(fill = 'white', color=NA),
+          plot.background = element_rect(fill = 'white', color=NA))
+  
+  
+  ggsave(paste("./OUT/VennDiagram_",sp,"_hvols_v",vs,".png",sep=""), 
+         plot = ggv, width = width, height = height)
+  
+}
+
+
+biplotHvol <- function(hv, sp, vs, colors = c('#e41a1c','#377eb8','#4daf4a'), 
+                       width = 4000, height = 2700, res=300){
+  
+  png(filename = paste("./OUT/HvolBiplot_",sp,"_hvols_v",vs,".png",sep=""), 
+      width = width, height = height, res = res)
+  
+  plot(hv,
+       #colors = c("#EBE300", "#0058FA", "#49A32D"),
+       #colors = c("yellow", "dark blue", "dark green"),
+       colors = colors,
+       point.alpha.min=0.3, 
+       point.dark.factor=0.4,
+       cex.names=1.5,
+       cex.legend=1.5,
+       cex.data = 1.5,
+       cax.random = 1.1,
+       num.points.max.data = 1000, 
+       num.points.max.random = 2000)
+  dev.off()
+  
+  
+}
+
+
 ptb <- vect("./DATA/VECTOR/PT_caop2020_bounds_WGS84.shp")
 
 
@@ -237,28 +301,127 @@ for(sp in spNames){
 }
 
 
+## -----------------------------------------------------------------------------------------##
+
+sps <- c("Lucanus cervus",
+         "Lucanus barbarossa",
+         "Dorcus parallelipipedus",
+         "Platycerus spinifer")
+
+spNames <- rep(sps, each=3)
+
+dataSources <- rep(c("REFS","CTSC", "ALL"),4)
+
+
+hv_volumes <- data.frame(spName = spNames, dtSource = dataSources, hvol = NA)
+
+
+hv_fn <- list.files("./OUT/hvol_v3/", pattern=".rds$", full.names = TRUE, recursive = TRUE)
+
+hypervols <- list()
+
+i <- 0
+for(fn in hv_fn){
+  i <- i + 1
+  
+  hv <- readr::read_rds(hv_fn[i])
+  hypervols[[spNames[i]]][[dataSources[i]]] <- hv
+  
+  hv_volumes[i,3] <- hv@Volume
+  
+}
+
+hv_lc <- hypervolume_join(hypervols[[1]][[1]],
+                          hypervols[[1]][[2]],
+                          hypervols[[1]][[3]])
+
+hv_lb <- hypervolume_join(hypervols[[2]][[1]],
+                          hypervols[[2]][[2]],
+                          hypervols[[2]][[3]])
+
+hv_dp <- hypervolume_join(hypervols[[3]][[1]],
+                          hypervols[[3]][[2]],
+                          hypervols[[3]][[3]])
+
+hv_ps <- hypervolume_join(hypervols[[4]][[1]],
+                          hypervols[[4]][[2]],
+                          hypervols[[4]][[3]])
+
+#?plot.Hypervolume()
+
+# png(filename = paste("./OUT/HvolBiplot_",sps[1],"_hvols_v3.png",sep=""), width = 4000, height = 2700, res=300)
+# plot(hv_lc,
+#      colors = c("#EBE300", "#0058FA", "#49A32D"),
+#      #colors = c("yellow", "dark blue", "dark green"), 
+#      point.alpha.min=0.3, 
+#      point.dark.factor=0.4,
+#      cex.names=1.5,
+#      cex.legend=1.5,
+#      cex.data = 1.5,
+#      cax.random = 1.1,
+#      num.points.max.data = 1000, 
+#      num.points.max.random = 2000)
+# dev.off()
+
+biplotHvol(hv=hv_lc, sp=sps[1], vs=3.1, width = 4000, height = 2700, res=300)
+biplotHvol(hv=hv_lb, sp=sps[2], vs=3.1, width = 4000, height = 2700, res=300)
+biplotHvol(hv=hv_dp, sp=sps[3], vs=3.1, width = 4000, height = 2700, res=300)
+biplotHvol(hv=hv_ps, sp=sps[4], vs=3.1, width = 4000, height = 2700, res=300)
+
+
+
+ggVennDiagHvols(hypervols[[1]][[1]],
+                hypervols[[1]][[2]], 
+                sp = sps[1], vs=3, width = 6, height= 4, tl = 10000)
+ggVennDiagHvols(hypervols[[2]][[1]],
+                hypervols[[2]][[2]], 
+                sp = sps[2], vs=3, width = 6, height= 4, tl = 10000)
+ggVennDiagHvols(hypervols[[3]][[1]],
+                hypervols[[3]][[2]], 
+                sp = sps[3], vs=3, width = 6, height= 4, tl = 10000)
+ggVennDiagHvols(hypervols[[4]][[1]],
+                hypervols[[4]][[2]], 
+                sp = sps[4], vs=3, width = 6, height= 4, tl = 10000)
+
 
 
 ## -----------------------------------------------------------------------------------------##
 
-outDF$DataType <- c("Other sources","Citizen science")
 
-outDF$DataType <- factor(outDF$DataType, 
-                         levels = c("Other sources","Citizen science"))
 
-g <- ggplot(outDF,
-            aes(y=svmHvol, x=DataType, fill=DataType)) + 
-  geom_bar(stat="identity") +
+# outDF$DataType <- c("Other sources","Citizen science")
+# 
+# outDF$DataType <- factor(outDF$DataType, 
+#                          levels = c("Other sources","Citizen science"))
+# 
+# g <- ggplot(outDF,
+#             aes(y=svmHvol, x=DataType, fill=DataType)) + 
+#   geom_bar(stat="identity") +
+#   xlab("Data sources") + 
+#   ylab("Niche hypervolume size") +
+#   facet_wrap(spName~.) + 
+#   scale_fill_discrete(name="Data sources") +
+#   labs(title = "Niche hypervolume size by data source") + 
+#   theme_bw()
+# plot(g)
+# 
+# ggsave("./OUT/HVolSize_BeforeAfterCSdatacoll_v2.png",plot = g,width = 8,height = 8)
+
+
+g <- ggplot(hv_volumes,
+            aes(y=hvol, x=dtSource, fill=dtSource)) + 
+  geom_bar(stat="identity", color="black", alpha=0.65, width=0.7) +
   xlab("Data sources") + 
   ylab("Niche hypervolume size") +
   facet_wrap(spName~.) + 
-  scale_fill_discrete(name="Data sources") +
   labs(title = "Niche hypervolume size by data source") + 
-  theme_bw()
+  scale_fill_brewer(palette = "Set1", name="Data source") +
+  theme_bw() + 
+  theme(legend.position = "bottom", 
+        text = element_text(size = 16))
 plot(g)
 
-ggsave("./OUT/HVolSize_BeforeAfterCSdatacoll_v2.png",plot = g,width = 8,height = 8)
-
+ggsave("./OUT/HVolSize_BeforeAfterCSdatacoll_v3.png",plot = g,width = 8,height = 8)
 
 
 
